fdbe31bc1fc7e7ccb3b0c6fbd19fab491c4a1dce
ilham <ilham.hanif@hreasily.com>
2023-09-15 00:14:00 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,31 @@
 import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    svgLoader(),
-  ],
-  server: {
-    host: true,
-    port: 8000,
-    watch: {
-      usePolling: true
-    },
-  },
-  resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      svgLoader(),
     ],
-  },
-});
+    server: {
+      host: true,
+      port: process.env.VITE_SERVER_PORT,
+      watch: {
+        usePolling: true
+      },
+    },
+    resolve: {
+      alias: [
+        { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      ],
+    },
+  });
+}
